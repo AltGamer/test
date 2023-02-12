505e93a9a414e914b049955efa9db72266a355c6
Artem Kriuchko <Artem_Kriuchko@epam.com>
2023-02-12 13:02:14 +0200

    Setting up view

--- src/main/java/com/article/demo/controller/HomePageController.java
+++ src/main/java/com/article/demo/controller/HomePageController.java
@@ -3,14 +3,17 @@ package com.article.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(path = "/home")
 public class HomePageController {
 
     @GetMapping
-    public String getHome() {
-        return "It is Wednesday My Dudes https://reactor.cc/post/5462363";
+    public ModelAndView getHome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
     }
 
 }
